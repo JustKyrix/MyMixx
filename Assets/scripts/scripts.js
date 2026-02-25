4dae5fad05d0cc613b8acac9093828d6b38a50e2
JustKyrix <kyrixfx@outlook.de>
2026-02-25 23:38:42 +0100

    stuff

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -5,3 +5,18 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('open');
   collapse.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const nav = document.getElementById('navbar');
+  const waveBg = document.querySelector('.nav-wave');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 500) {
+      nav.classList.add('sticky', 'nav-bg');
+      waveBg.style.display = 'none';
+    } else {
+      nav.classList.remove('sticky', 'nav-bg');
+      waveBg.style.display = 'block';
+    }
+  });
+});
