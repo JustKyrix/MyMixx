0485883e828b698d1433e14ea173326d0a44ee35
JustKyrix <kyrixfx@outlook.de>
2026-02-26 16:59:20 +0100

    laptop commits

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,22 +1,44 @@
-const toggle   = document.querySelector('.navbar-toggle');
+const toggle = document.querySelector('.navbar-toggle');
 const collapse = document.querySelector('.navbar-collapse');
+const nav = document.getElementById('navbar');
+const h1 = document.querySelector('h1');
+const breakpoint = window.matchMedia('(max-width: 991px)');
+const body = document.querySelector('body');
+
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('open');
   collapse.classList.toggle('open');
+  nav.classList.toggle('nav-bg'); 
+
+  if (collapse.classList.contains('open')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'initial';
+    body.style.overflowX = 'hidden';
+  }
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-  const nav = document.getElementById('navbar');
-  const waveBg = document.querySelector('.nav-wave');
+  function handleBreakpoint(e) {
+    if (e.matches) {
+      h1.classList.add('h2');
+    } else {
+      h1.classList.remove('h2');
+    }
+  }
+
+  // Listen for changes
+  breakpoint.addEventListener('change', handleBreakpoint);
+
+  // Run once on load
+  handleBreakpoint(breakpoint);
 
   window.addEventListener('scroll', function() {
     if (window.scrollY > 500) {
-      nav.classList.add('sticky', 'nav-bg');
-      waveBg.style.display = 'none';
+      nav.classList.add('sticky');
     } else {
-      nav.classList.remove('sticky', 'nav-bg');
-      waveBg.style.display = 'block';
+      nav.classList.remove('sticky');
     }
   });
 });
